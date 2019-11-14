8ed3ce3e69223ce0721e1123528ef4327ccbbf45
Yevhenii Hyzyla <hyzyla@MacBook-Pro-Olena.local>
2019-11-14 23:00:18 +0200

    Add base handlers

--- app/handlers.py
+++ app/handlers.py
@@ -1,15 +1,35 @@
 from enum import Enum
 
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     MessageHandler,
     Filters,
     CommandHandler,
     CallbackContext,
     ConversationHandler,
+    CallbackQueryHandler,
 )
 
 from app import updater
+
+CITIES = [
+    ('Київ', 1),
+    ('Полтава', 2),
+    ('Одеса', 3),
+    ('Львів', 4),
+    ('Вінния', 5),
+    ('Харків', 6),
+    ('Донецьк', 7),
+]
+
+
+MENU = """
+*Список підтримуваних команд:*
+ - /start - почати роботу з ботом
+ - /list - список підписок
+ - /add - підписатися на нові сповіщення
+ - /help - отримати довідку
+"""
 
 
 class AddSubscriptionStates(Enum):
@@ -17,27 +37,65 @@ class AddSubscriptionStates(Enum):
     position = 'position'
 
 
+def __send_help(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        'Спиос'
+    )
+
+
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
-        'Привіт я телеграм бот який буде шукати роботу за '
-        'тебе. \n'
-        'Ось список  ',
+        'Привіт я телеграм бот який буде шукати роботу за тебе.'
+        '\n'
+        '{}'.format(MENU),
+        parse_mode='Markdown',
     )
 
 
-def menu(update: Update, context: CallbackContext):
-    update.message.reply(
-        'Привіт я телеграм бот який буде шукати роботу за '
-        'тебе. \n'
-        'Ось список  ',
-    )
+def help_(update: Update, context: CallbackContext):
+    update.message.reply_text(MENU, parse_mode='Markdown')
 
 
 def add_subscription(update: Update, context: CallbackContext):
     update.message.reply_text(
-        "Введіть місто"
+        "Для підпсання на нові вакансії потрібно пройти невелике опитування. "
+        "Якщо бажаєте відхили опитування введіть команду /cancel"
+    )
+
+    keyboards = [
+        [InlineKeyboardButton(name, callback_data=f'city.{id_}')]
+        for name, id_ in CITIES
+    ]
+    keyboards.append([
+        InlineKeyboardButton('<', callback_data='city.prev'),
+        InlineKeyboardButton('>', callback_data='city.next'),
+    ])
+    update.message.reply_text(
+        "Вкажіть місто де потрібно шукати вакансії",
+        reply_markup=InlineKeyboardMarkup(keyboards)
     )
     return AddSubscriptionStates.city
+
+
+def add_subscription_city(update: Update, context: CallbackContext):
+    data: str = update.callback_query.data
+    _, suffix = data.strip().split('.')
+
+    update.message.reply_text(
+       f"Місто {suffix} додано, тепер додайте позицію"
+    )
+    return AddSubscriptionStates.position
+
+
+def add_subscription_city_navigate(update: Update, context: CallbackContext):
+    data: str = update.callback_query.data
+    _, suffix = data.strip().split('.')
+    if suffix == 'prev':
+        pass
+        return
+    if suffix == 'next':
+        pass
+        return
 
 
 def add_subscription_fallback(update: Update, context: CallbackContext):
@@ -47,23 +105,16 @@ def add_subscription_fallback(update: Update, context: CallbackContext):
     return ConversationHandler.END
 
 
-def add_subscription_city(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Місто додано, тепер додайте позицію"
-    )
-    return AddSubscriptionStates.position
-
-
 def add_subscription_position(update: Update, context: CallbackContext):
     update.message.reply_text(
-        "Позицію додано, тепер додайте позицію"
+        "Супер, тепер я буду сповіщати тебе про нові вакансії"
     )
     return ConversationHandler.END
 
 
 def list_subscription(update: Update, context: CallbackContext):
     update.message.reply_text(
-        "Ось список твоїх підписок"
+        "Список підписок"
     )
 
 
@@ -81,7 +132,8 @@ dp.add_handler(
         entry_points=[CommandHandler('add', add_subscription)],
         states={
             AddSubscriptionStates.city: [
-                MessageHandler(Filters.text, add_subscription_city),
+                CallbackQueryHandler(add_subscription_city_navigate, pattern=r'city\.(prev|next)'),
+                CallbackQueryHandler(add_subscription_city, pattern=r'city\.\d+')
             ],
             AddSubscriptionStates.position: [
                 MessageHandler(Filters.text, add_subscription_position),
@@ -93,5 +145,6 @@ dp.add_handler(
     )
 )
 dp.add_handler(CommandHandler('list', list_subscription))
+dp.add_handler(CommandHandler('help', help_))
 
 dp.add_handler(MessageHandler(Filters.text, fallback))
